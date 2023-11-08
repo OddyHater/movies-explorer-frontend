d21b649068370b0202da34345026f35a95180b34
Roman Grishin <roman.grishin.me@gmail.com>
2023-11-08 11:16:52 +0600

    saved movies page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,13 +3,15 @@ import React from "react";
 import Main from "../../pages/Main/Main";
 import Movies from "../../pages/Movies/Movies";
 import NotFound from "../../pages/NotFound/NotFound";
+import SavedMovies from "../../pages/SavedMovies/SavedMovies";
 
 function App() {
   return (
     <>
       {/* <Main /> */}
-      <Movies />
+      {/* <Movies /> */}
       {/* <NotFound /> */}
+      <SavedMovies />
     </>
   );
 }
--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,92 +1,16 @@
 import React from 'react';
 
 import './movies-cards.css';
-import CardImage from '../../images/card-image.png'
 
 import Wrapper from '../Wrapper/Wrapper';
 import MoviesCard from '../MoviesCard/MoviesCard';
 
-const MoviesCardList = () => {
-
-  const cardArray = [
-    {
-      id: 1,
-      duration: 120,
-      thumbnail: CardImage,
-      nameRU: 'В погоне за Бенкси'
-    },
-    {
-      id: 2,
-      duration: 42,
-      thumbnail: CardImage,
-      nameRU: 'В погоне за Бенкси'
-    },
-    {
-      id: 3,
-      duration: 42,
-      thumbnail: CardImage,
-      nameRU: 'В погоне за Бенкси'
-    },
-    {
-      id: 4,
-      duration: 42,
-      thumbnail: CardImage,
-      nameRU: 'В погоне за Бенкси'
-    },
-    {
-      id: 5,
-      duration: 42,
-      thumbnail: CardImage,
-      nameRU: 'В погоне за Бенкси'
-    },
-    {
-      id: 6,
-      duration: 42,
-      thumbnail: CardImage,
-      nameRU: 'В погоне за Бенкси'
-    },
-    {
-      id: 7,
-      duration: 120,
-      thumbnail: CardImage,
-      nameRU: 'В погоне за Бенкси'
-    },
-    {
-      id: 8,
-      duration: 42,
-      thumbnail: CardImage,
-      nameRU: 'В погоне за Бенкси'
-    },
-    {
-      id: 9,
-      duration: 42,
-      thumbnail: CardImage,
-      nameRU: 'В погоне за Бенкси'
-    },
-    {
-      id: 10,
-      duration: 42,
-      thumbnail: CardImage,
-      nameRU: 'В погоне за Бенкси'
-    },
-    {
-      id: 11,
-      duration: 42,
-      thumbnail: CardImage,
-      nameRU: 'В погоне за Бенкси'
-    },
-    {
-      id: 12,
-      duration: 42,
-      thumbnail: CardImage,
-      nameRU: 'В погоне за Бенкси'
-    },
-  ]
+const MoviesCardList = ({ cardsArray }) => {
 
   return (
     <Wrapper modifier={'movies-cards'}>
       <ul className="movies-cards">
-        {cardArray.map((card) => (
+        {cardsArray.map((card) => (
           <MoviesCard
             key={card.id}
             duration={card.duration}
